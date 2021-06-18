eb786b0c4815c2296f3b43b6a964a10a7cf34caf
Doug <doug.kerzner@gmail.com>
2021-06-17 20:07:42 -0500

    Tested new font, no longer kicking off error but does not appear to be changing correctly

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,43 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+// import * as React from 'react';
+// import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import { Text, View } from '../components/Themed';
+// import {
+//   useFonts,
+//   NotoSansKR_100Thin,
+//   NotoSansKR_300Light,
+//   NotoSansKR_400Regular,
+//   NotoSansKR_500Medium,
+//   NotoSansKR_700Bold,
+//   NotoSansKR_900Black,
+// } from '@expo-google-fonts/noto-sans-kr';
+
+import React, { useState, useEffect } from 'react';
+ 
+import { Text, View, StyleSheet } from 'react-native';
+import { AppLoading } from 'expo';
+import {
+  useFonts,
+  NotoSansKR_100Thin,
+  NotoSansKR_300Light,
+  NotoSansKR_400Regular,
+  NotoSansKR_500Medium,
+  NotoSansKR_700Bold,
+  NotoSansKR_900Black,
+} from '@expo-google-fonts/noto-sans-kr';
 
 export default function TabOneScreen() {
+
+  let [fontsLoaded] = useFonts({
+    NotoSansKR_100Thin,
+    NotoSansKR_300Light,
+    NotoSansKR_400Regular,
+    NotoSansKR_500Medium,
+    NotoSansKR_700Bold,
+    NotoSansKR_900Black,
+  });
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One Pesh Test</Text>
@@ -20,11 +53,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: "NotoSansKR_700Bold"
   },
   title: {
     color: "#eee",
     fontSize: 20,
     fontWeight: 'bold',
+    fontFamily: "NotoSansKR_700Bold"
   },
   separator: {
     marginVertical: 30,
